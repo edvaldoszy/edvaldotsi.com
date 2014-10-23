ad74bbfc9780fc739d618aacd1ae395213893bea
Edvaldo Szymonek <edvaldoszy@gmail.com>
2014-10-23 12:16:10 -0200

    Arquivos do projeto

---
+++ site/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<title>Edvaldo Szymonek</title>
+	<meta name="description" content="BLZ - Soluções Web">
+	<meta name="keywords" content="blz, soluções, web, campo, mourao, curitiba, parana, hospedagem, central, imobiliarias, internet, banda, larga">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="robots" content="index,follow">
+	<link rel="shortcut icon" href="/web/img/favicon.png">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="/web/css/style.css" media="all">
+</head>
+<body>
+	<div class="wrap">
+		<header class="wrap-header responsive">
+			<div class="content">
+				<h1 class="text-center">Edvaldo Szymonek</h1>
+				<nav class="navbar responsive">
+					<ul class="menu">
+						<li><a href="#" title="Página inicial">Home</a></li>
+						<li><a href="#" title="Artigos, tutoriais e muito mais">Blog</a></li>
+						<li><a href="#" title="Sobre mim">Sobre</a></li>
+						<li><a href="#" title="Meu portifólio">Portifólio</a></li>
+						<li><a href="#" title="Entre em contato">Contato</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<main class="wrap-content responsive">
+			<div class="content">
+				<div class="services">
+					<div class="item">
+						<img class="circle" style="display: block; width: 200px; margin: 0 auto;" src="/web/img/tecnologia.jpg" alt="">
+					</div>
+					<div class="item"></div>
+					<div class="item"></div>
+				</div>
+			</div>
+			
+		</main>
+	</div>
+</body>
+</html>
