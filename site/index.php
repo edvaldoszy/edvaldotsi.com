b03f3640cb09ea2fd5e2f2c5f54d88e064c65b60
Edvaldo Szymonek <edvaldoszy@gmail.com>
2014-10-24 23:57:44 -0200

    Modificações na estrutura e estilo

--- site/index.php
+++ site/index.php
@@ -3,11 +3,17 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Edvaldo Szymonek</title>
-	<meta name="description" content="BLZ - Soluções Web">
-	<meta name="keywords" content="blz, soluções, web, campo, mourao, curitiba, parana, hospedagem, central, imobiliarias, internet, banda, larga">
+	<meta name="robots" content="index, follow">
+	<meta name="description" content="Edvaldo Szymonek - Soluções Web e Mobile">
+	<meta name="keywords" content="soluções, web, mobile, android, ios, internet">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="robots" content="index,follow">
-	<link rel="shortcut icon" href="/web/img/favicon.png">
+	<meta property="og:url" content="http://edvaldotsi.com">
+	<meta property="og:locale" content="pt_BR">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Edvaldo's personal website">
+	<meta property="og:site_name" content="Edvaldo Szymonek">
+	<meta property="og:description" content="Descrição do website para o Facebook">
+	<link rel="shortcut icon" href="/web/img/favicon.ico" type="image/x-icon">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="/web/css/style.css" media="all">
 </head>
@@ -15,44 +21,55 @@
 	<div class="wrap">
 		<header class="wrap-header responsive">
 			<div class="content">
-				<h1 class="text-center">Edvaldo Szymonek</h1>
+				<h1 class="text-center" title="Edvaldo Szymonek - Soluções Web e Mobile">Edvaldo Szymonek</h1>
 				<nav class="navbar responsive">
 					<ul class="menu">
 						<li><a href="/" title="Página inicial">Home</a></li>
 						<li><a href="/blog" title="Artigos, tutoriais e muito mais">Blog</a></li>
 						<li><a href="/sobre" title="Sobre mim">Sobre</a></li>
-						<li><a href="/portifolio" title="Meu portifólio">Portifólio</a></li>
+						<li><a href="/portifolio" title="Meus trabalhos">Portifólio</a></li>
 						<li><a href="/contato" title="Entre em contato">Contato</a></li>
 					</ul>
 				</nav><!-- .navbar -->
 			</div>
 		</header><!-- .wrap-header -->
-		<main class="wrap-content responsive">
-			<div class="content">
-				<div class="services text-center">
-					<div class="item">
-						<h2>Web</h2>
-						<img class="circle" src="/web/img/tecnologia.jpg" alt="">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus at culpa obcaecati, recusandae. Tenetur neque, ad quam cumque consequuntur modi ea officiis voluptatem architecto. Excepturi iure accusantium, doloribus fugiat earum.</p>
-						<a class="bt bt-more" href="#">Saiba mais</a>
-					</div>
-					<div class="item">
-						<h2>Mobile</h2>
-						<img class="circle" src="/web/img/tecnologia.jpg" alt="">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, optio voluptate sapiente hic cumque sunt harum porro dolorem blanditiis enim. Eligendi quos minima provident minus, laborum nihil repellendus, ipsa alias.</p>
-						<a class="bt bt-more" href="#">Saiba mais</a>
-					</div>
-					<div class="item">
-						<h2>Linux</h2>
-						<img class="circle" src="/web/img/tecnologia.jpg" alt="">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt inventore dolorum iste expedita vel odio doloremque corporis officia tenetur quam quae atque aperiam, in accusantium. Veritatis soluta natus sapiente, assumenda!</p>
-						<a class="bt bt-more" href="#">Saiba mais</a>
-					</div>
-					<div class="clear"></div>
-				</div> <!-- .services -->
+
+		<main class="wrap-main responsive">
+			<div class="container wrap-services responsive">
+				<div class="content">
+					<div class="services text-center">
+						<div class="item">
+							<h2>Web</h2>
+							<img class="circle" src="/web/img/tecnologia.jpg" alt="">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus at culpa obcaecati, recusandae. Tenetur neque, ad quam cumque consequuntur modi ea officiis voluptatem architecto. Excepturi iure accusantium, doloribus fugiat earum.</p>
+							<a class="bt bt-more" href="#">Saiba mais</a>
+						</div>
+						<div class="item">
+							<h2>Mobile</h2>
+							<img class="circle" src="/web/img/tecnologia.jpg" alt="">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, optio voluptate sapiente hic cumque sunt harum porro dolorem blanditiis enim. Eligendi quos minima provident minus, laborum nihil repellendus, ipsa alias.</p>
+							<a class="bt bt-more" href="#">Saiba mais</a>
+						</div>
+						<div class="item">
+							<h2>Linux</h2>
+							<img class="circle" src="/web/img/tecnologia.jpg" alt="">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt inventore dolorum iste expedita vel odio doloremque corporis officia tenetur quam quae atque aperiam, in accusantium. Veritatis soluta natus sapiente, assumenda!</p>
+							<a class="bt bt-more" href="#">Saiba mais</a>
+						</div>
+						<div class="clear"></div>
+					</div> <!-- .services -->
+				</div>
+			</div>
+
+			<div class="container wrap-mobile responsive">
+				<div class="content">
+					<h3 class="text-center">Mobile</h3>
+					<p></p>
+				</div>
 			</div>
 		</main><!-- .wrap-content -->
-		<footer class="wrap-footer">
+
+		<footer class="wrap-footer responsive">
 			<div class="content">
 				<nav class="links">
 					<ul>
@@ -60,14 +77,15 @@
 						<li><a href="/" title="Página inicial">Home</a></li>
 						<li><a href="/blog" title="Artigos, tutoriais e muito mais">Blog</a></li>
 						<li><a href="/sobre" title="Sobre mim">Sobre</a></li>
-						<li><a href="/portifolio" title="Meu portifólio">Portifólio</a></li>
+						<li><a href="/portifolio" title="Meus trabalhos">Portifólio</a></li>
 						<li><a href="/contato" title="Entre em contato">Contato</a></li>
 					</ul>
 					<ul>
 						<li class="font-bold">Redes sociais</li>
-						<li><a href="http://facebook.com/edvaldoszy" target="_blank" title="Curta nossa página no Facebook">Facebook</a></li>
-						<li><a href="http://plus.google.com/" target="_blank" title="Siga-nos no Google Plus">Google Plus</a></li>
-						<li><a href="http://twitter.com" target="_blank" title="Siga-nos no Twitter">Twitter</a></li>
+						<li><a href="https://facebook.com/edvaldoszy" target="_blank" title="Curta nossa página no Facebook">Facebook</a></li>
+						<li><a href="https://plus.google.com/u/0/102934675252501071967" target="_blank" title="Siga-nos no Google Plus">Google Plus</a></li>
+						<li><a href="https://twitter.com/edvaldoszy" target="_blank" title="Siga-nos no Twitter">Twitter</a></li>
+						<li><a href="https://youtube.com/dredvaldo" target="_blank" title="Vídeos e tutoriais">YouTube</a></li>
 					</ul>
 					<ul>
 						<li class="font-bold">Comunidade</li>
